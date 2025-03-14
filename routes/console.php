d645349cdf5bd8379694267c59c684d38116f996
Jazeel Zainudeen <zainudheenjazeel@gmail.com>
2025-03-15 01:49:43 +0530

    feat: schedule FetchNewsArticles command to run every thirty minutes

--- routes/console.php
+++ routes/console.php
@@ -1,10 +1,6 @@
 <?php
 
-use Illuminate\Foundation\Console\ClosureCommand;
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
+use App\Console\Commands\FetchNewsArticles;
+use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    /** @var ClosureCommand $this */
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+Schedule::command(FetchNewsArticles::class)->everyThirtyMinutes();
